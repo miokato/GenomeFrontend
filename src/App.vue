dda4dc3cb2696e80d55cb44932fba2e9a3b27831
miokato <miokato@me.com>
2018-05-26 23:26:25 +0900

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -6,38 +6,55 @@
       <div id="title" class="hero-body">
         <div class="container">
           <h1 class="title">
-            GenoGuru
+            Genoguru
           </h1>
           <h2 class="subtitle">
-            Food recommendation system by Genome map
+            好みが合う人がオススメする美味しいお店
           </h2>
         </div>
       </div>
     </section>
 
 
-    <!-- search bar -->
+    <!-- search form -->
     <section class="search-bar">
       <div class="container">
+
         <div class="field has-addons">
           <div class="control">
-            <input @keyup.enter="search" v-model="query"
-                   class="input" type="text" placeholder="Search">
+            <input @keyup.enter="search" v-model="query" class="input" type="text" placeholder="食べたい料理">
           </div>
           <div class="control">
             <a @click="search" class="button is-warning">
-              Search
+              検索
             </a>
           </div>
         </div>
       </div>
+
     </section>
 
 
     <!-- table -->
-    <section>
-      <b-table :data="data" :columns="columns"></b-table>
+    <section id="table">
+      <div class="container">
+        <b-table :data="data" :columns="columns"></b-table>
+      </div>
     </section>
+
+
+    <!-- footer -->
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Genoguru</strong> by <a href="https://jgthms.com">Team X</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          </p>
+        </div>
+      </div>
+    </footer>
 
   </div>
 </template>
@@ -78,7 +95,7 @@
     },
     methods: {
       search: function () {
-        axios.get(`http://api.zipaddress.net/?zipcode=${this.query}`)
+        axios.get(`https://api.zipaddress.net/?zipcode=${this.query}`)
           .then(response => {
             this.data = [response.data.data];
 
@@ -96,5 +113,9 @@
   .search-bar {
     margin-top: 60px;
     margin-bottom: 30px;
+  }
+
+  #table {
+    margin-bottom: 300px;
   }
 </style>
